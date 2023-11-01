0efcceee84876288ae7e6d7764c79a8e568e8e65
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-11-01 04:44:28 -0400

    add exercise card with completion visuals

--- app/src/main/java/com/example/assignment3/components/ExerciseCard.kt
+++ app/src/main/java/com/example/assignment3/components/ExerciseCard.kt
@@ -1,4 +1,5 @@
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,6 +10,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,19 +23,24 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.assignment3.R
-import com.example.assignment3.navigation.Routes
+import com.example.compose.md_theme_light_primary
 import com.example.kotlinwithcompose.screens.LocalNavController
 
 @Composable
-fun WorkoutCard() {
+fun ExerciseCard() {
     val navController = LocalNavController.current
+    var isToggled by remember { mutableStateOf(false) }
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(5.dp)
             .border(5.dp, color = Color.Black, RoundedCornerShape(16.dp))
             .padding(5.dp)
-            .clickable { navController.navigate(Routes.Exercises.route) },
+            .background(
+                if (!isToggled) Color.White else md_theme_light_primary
+            )
+            .clickable { isToggled = !isToggled }
 
     ) {
         Column(
@@ -39,12 +49,12 @@ fun WorkoutCard() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Yoga",
+                text = "Starfish",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.SemiBold,
             )
-            Text(text = "16 exercises")
-            Text(text = "45 minutes")
+            Text(text = "3 sets")
+            Text(text = "5 minutes each")
         }
         Image(
             painter = painterResource(id = R.drawable.chocolate),
--- app/src/main/java/com/example/assignment3/screen/ExercisesScreen.kt
+++ app/src/main/java/com/example/assignment3/screen/ExercisesScreen.kt
@@ -1,23 +1,53 @@
 package com.example.assignment3.screen
 
+import ExerciseCard
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.compose.md_theme_light_primary
 
 
 @Composable
 fun ExercisesScreen() {
-    Column(modifier = Modifier.padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = "Exercises",
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold,
-        )
+    Column(
+        modifier = Modifier
+            .padding(10.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+       Column(Modifier.fillMaxWidth(),horizontalAlignment = Alignment.CenterHorizontally) {
+           Text(
+               text = "Yoga",
+               fontSize = 40.sp,
+               fontWeight = FontWeight.Bold,
+           )
+           Text(
+               text = "4 exercises",
+               fontSize = 20.sp,
+               fontWeight = FontWeight.Bold,
+           )
+           Text(
+               text = "Completed",
+               fontSize = 20.sp,
+               fontWeight = FontWeight.Bold,
+               textAlign = TextAlign.Center,
+               modifier = Modifier.background(color = md_theme_light_primary)
+           )
+       }
+
+        ExerciseCard()
+        ExerciseCard()
+        ExerciseCard()
+        ExerciseCard()
     }
 }
