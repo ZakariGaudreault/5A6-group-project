cf7b417b56abf16452680677533b496b66415093
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-11-14 05:27:35 -0500

    fix ktlint styling issues

--- app/src/main/java/com/example/snapfit/navigation/SharedBottomBar.kt
+++ app/src/main/java/com/example/snapfit/navigation/SharedBottomBar.kt
@@ -14,7 +14,6 @@ import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.snapfit.R
 
-
 /**
  * The default bottom nav bar with three elements : Home button(which is the list of the deathbeds), about button, and an add option button
  */
@@ -28,15 +27,16 @@ fun SharedBottomBar() {
         data class NavBarIcon(
             val route: String,
             val icon: ImageVector? = null,
-            val iconResId: Int
+            val iconResId: Int,
         )
 
         // List of items routes in a NavBarIcon class, to be used in the bottom navbar
-        val items = listOf(
-            NavBarIcon(route = Routes.Main.route, iconResId = R.drawable.house),
-            NavBarIcon(route = Routes.Workouts.route, iconResId = R.drawable.workout),
-            NavBarIcon(route = Routes.Profile.route, iconResId = R.drawable.gear),
-        )
+        val items =
+            listOf(
+                NavBarIcon(route = Routes.Main.route, iconResId = R.drawable.house),
+                NavBarIcon(route = Routes.Workouts.route, iconResId = R.drawable.workout),
+                NavBarIcon(route = Routes.Profile.route, iconResId = R.drawable.gear),
+            )
 
         items.forEachIndexed { index, item ->
             NavigationBarItem(
@@ -44,12 +44,14 @@ fun SharedBottomBar() {
                     Image(
                         painter = painterResource(id = item.iconResId),
                         contentDescription = item.route,
-                        modifier = Modifier.size(24.dp) // Adjust size as needed
+                        // Adjust size as needed
+                        modifier = Modifier.size(24.dp),
                     )
                 },
-                selected = currentDestination?.hierarchy?.any {
-                    currentDestination.route?.substringBefore('/') == item.route.substringBefore('/')
-                } == true,
+                selected =
+                    currentDestination?.hierarchy?.any {
+                        currentDestination.route?.substringBefore('/') == item.route.substringBefore('/')
+                    } == true,
                 onClick = { navController.navigate(item.route) },
             )
         }
