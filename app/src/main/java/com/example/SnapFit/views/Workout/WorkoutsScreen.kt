bf97bb0631107ac6931f08cd0029af35324d5016
ZakariGaudreault <98484664+ZakariGaudreault@users.noreply.github.com>
2023-12-04 15:51:46 -0500

    filter and slidebar

--- app/src/main/java/com/example/SnapFit/views/Workout/WorkoutsScreen.kt
+++ app/src/main/java/com/example/SnapFit/views/Workout/WorkoutsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.snapfit.views.workout
 
 import WorkoutCard
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
@@ -15,21 +17,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.snapfit.R
 
 @Composable
 fun WorkoutsScreen() {
+    var homeworkout by rememberSaveable { mutableStateOf(true) }
+    var gymworkout by rememberSaveable { mutableStateOf(true) }
+    var outdoorworkout by rememberSaveable { mutableStateOf(true) }
     var sliderPosition by remember { mutableFloatStateOf(0f) }
+    val backgroundColorHome = if (homeworkout) colorResource(R.color.purple_500) else colorResource(R.color.purple_200)
+    val backgroundColorGym = if (gymworkout) colorResource(R.color.purple_500) else colorResource(R.color.purple_200)
+    val backgroundColorOutdoor = if (outdoorworkout) colorResource(R.color.purple_500) else colorResource(R.color.purple_200)
+
     Column(
         modifier =
-            Modifier
-                .padding(10.dp)
-                .verticalScroll(rememberScrollState()),
+        Modifier
+            .padding(10.dp)
+            .verticalScroll(rememberScrollState()),
     ) {
         Text(
             text = "Workouts",
@@ -52,31 +67,38 @@ fun WorkoutsScreen() {
 
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.padding(vertical = 10.dp),
+            modifier = Modifier.padding(vertical = 10.dp).padding(horizontal = 42.dp),
+
         ) {
             Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.weight(1f),
+                onClick = { homeworkout = !homeworkout },
+                colors = buttonColors(backgroundColorHome)
             ) {
                 Text("Home")
             }
             Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.weight(1f),
+                onClick = { gymworkout = !gymworkout },
+                colors = buttonColors(backgroundColorGym)
             ) {
                 Text("Gym")
             }
             Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.weight(1f),
+                onClick = { outdoorworkout = !outdoorworkout },
+                colors = buttonColors(backgroundColorOutdoor)
             ) {
                 Text("Outdoor")
             }
         }
 
+
         WorkoutCard("cardio")
         WorkoutCard("strength")
-        WorkoutCard("no equipment")
+
+        if(homeworkout) {
+            WorkoutCard("no equipment")
+        }
+
+        if(sliderPosition.toInt() > 45)
         WorkoutCard("flexibility")
     }
 }
