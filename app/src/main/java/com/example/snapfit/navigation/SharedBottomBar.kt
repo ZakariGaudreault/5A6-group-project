82c188ac7fc05ecd147085d0e6a76f5da4b1e25e
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-12-10 18:12:29 -0500

    add navigation icons

--- app/src/main/java/com/example/snapfit/navigation/SharedBottomBar.kt
+++ app/src/main/java/com/example/snapfit/navigation/SharedBottomBar.kt
@@ -35,7 +35,9 @@ fun SharedBottomBar() {
             listOf(
                 NavBarIcon(route = Routes.Main.route, iconResId = R.drawable.house),
                 NavBarIcon(route = Routes.Workouts.route, iconResId = R.drawable.workout),
-                NavBarIcon(route = Routes.Profile.route, iconResId = R.drawable.gear),
+                NavBarIcon(route = Routes.Snap.route, iconResId = R.drawable.circle),
+                NavBarIcon(route = Routes.About.route, iconResId = R.drawable.info),
+                NavBarIcon(route = Routes.Workouts.route, iconResId = R.drawable.gear),
             )
 
         items.forEachIndexed { index, item ->
